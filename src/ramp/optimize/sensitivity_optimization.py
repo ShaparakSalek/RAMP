0bdf1ad5af3c6ec3590c5e6bc64c27e79f27c423
Tian <tian7@ml-5562150.the-lab.llnl.gov>
2023-10-31 17:30:12 -0700

    simplify python class

--- src/ramp/optimize/sensitivity_optimization.py
+++ src/ramp/optimize/sensitivity_optimization.py
@@ -20,177 +20,18 @@ import matplotlib.ticker as mticker
 from matplotlib.colors import LinearSegmentedColormap
 # from ipywidgets import interact, IntSlider, HBox, IntText, BoundedIntText
 # import ipywidgets as widgets
-import yaml
+
 import argparse
-from obspy import  Trace, Stream, UTCDateTime
-from obspy.core import AttribDict
-from obspy.io.segy.segy import SEGYTraceHeader, SEGYBinaryFileHeader
 import numpy as np
-from obspy.io.segy.core import _read_segy
-import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utilities.data_readers.read_input_from_files import read_yaml_parameters,read_sens_from_segy
+from utilities.write_output_to_files import convert_matrix_to_segy,write_optimal_design_to_yaml
 mpl.rcParams.update({'font.size': 20})
 DEBUG = False
 
 
 
 # -------------------------------
-
-def init_seis_from_yaml(yaml_path):
-    """
-    Initializes seismic parameters from a YAML file.
-
-    Parameters:
-        yaml_path (str): Path to the input YAML file.
-
-    Returns:
-        seis (object): Seismic sensitivity object with parameters set according to the YAML file.
-    """
-    parameters = read_yaml_parameters(yaml_path)
-    # read in parameters from YAML file
-    nx = parameters['nx']
-    nz = parameters['nz']
-    dz = parameters['dz']
-    dx = parameters['dx']
-    ns = parameters['ns']
-    nr = parameters['nr']
-    dr = parameters['dr']
-    ds = parameters['ds']
-    t1 = parameters['t1']
-    years0 = parameters['years']
-    senMax = parameters['senMax']
-    thresholds = parameters['thresholds']
-    ks = parameters['ks']
-    kr = parameters['kr']
-    sen_nor=parameters['sen_nor']
-    wavefield = parameters['wavefield']
-    vpvs = parameters['vpvs']
-    units= parameters['units']
-    datadir = parameters['datadir']
-    outpre = parameters['outpre']
-    years = [str(y) for y in years0]
-    dtc_flag=parameters['dtc_flag']
-    output_yaml=parameters['output_yaml']
-    target_dtc=parameters['target_dtc']
-    segy_read=parameters['segy_read']
-    # create a sensitivity object
-    seis = MonitoringDesignSensitivity2D(nx,nz,dx,dz,ns,nr,ds,dr,years,t1,thresholds,ks,kr,
-                                         sen_nor,datadir,outpre,wavefield,vpvs,units,dtc_flag,output_yaml,target_dtc,segy_read)
-    return seis
-
-
-def read_yaml_parameters(file_path):
-    """
-    Reads and flattens parameters from a given YAML file.
-
-    Parameters:
-        file_path (str): Path to the input YAML file.
-
-    Returns:
-        flattened_params (dict): Flattened dictionary of parameters read from the YAML file.
-    """
-    with open(file_path, 'r') as file:
-        params = yaml.safe_load(file)
-    # Flattening the restructured YAML content
-    flattened_params = {}
-    for section, values in params.items():
-        if isinstance(values, dict):  # Adding a check for dictionary items
-            for key, value in values.items():
-                flattened_params[key] = value
-        else:
-            flattened_params[section] = values
-    return flattened_params
-
-def convert_matrix_to_segy(sens2d,filename):
-    """
-    Converts a 2D matrix into a SEGY file format.
-
-    Parameters:
-        sens2d (np.array): 2D array representing the sensitivity matrix.
-        filename (str): Name of the output SEGY file.
-
-    Returns:
-        None. Writes the converted data to a SEGY file.
-    """
-    stream = Stream()
-
-    for i in range(sens2d.shape[0]):
-        # Create some random data.
-        data = sens2d[i][:]
-        data = np.require(data, dtype=np.float32)
-        trace = Trace(data=data)
-
-        # Attributes in trace.stats will overwrite everything in
-        # trace.stats.segy.trace_header
-        trace.stats.delta = 0.01
-        # SEGY does not support microsecond precision! Any microseconds will
-        # be discarded.
-        trace.stats.starttime = UTCDateTime(2011,11,11,11,11,11)
-
-        # If you want to set some additional attributes in the trace header,
-        # add one and only set the attributes you want to be set. Otherwise the
-        # header will be created for you with default values.
-        if not hasattr(trace.stats, 'segy.trace_header'):
-            trace.stats.segy = {}
-        trace.stats.segy.trace_header = SEGYTraceHeader()
-        trace.stats.segy.trace_header.trace_sequence_number_within_line = i + 1
-        trace.stats.segy.trace_header.receiver_group_elevation = 444
-
-        # Add trace to stream
-        stream.append(trace)
-
-    # A SEGY file has file wide headers. This can be attached to the stream
-    # object.  If these are not set, they will be autocreated with default
-    # values.
-    stream.stats = AttribDict()
-    stream.stats.textual_file_header = 'Textual Header!'
-    stream.stats.binary_file_header = SEGYBinaryFileHeader()
-    stream.stats.binary_file_header.trace_sorting_code = 5
-    stream.write(filename, format="SEGY", data_encoding=1,
-                byteorder=sys.byteorder)
-
-
-def read_sens_from_segy(filename,sen_nor):
-    """
-    Reads sensitivity values from a SEGY file and normalizes them if required.
-
-    Parameters:
-        filename (str): Path to the input SEGY file.
-        sen_nor (int): Flag to determine if sensitivity values need to be normalized.
-
-    Returns:
-        sens (np.array): 2D array of sensitivity values.
-    """
-    st1 = _read_segy(filename)
-    sens2d_read=[]
-    for i,tr in enumerate(st1):
-        sens2d_read.append(tr.data)
-    sens=np.array(sens2d_read)
-    if sen_nor==1:
-        maxSens = np.max(sens)
-        minSens = np.min(sens)
-        dsens = maxSens - minSens
-        sens = (sens - minSens) / dsens
-    return sens
-
-def write_optimal_design_to_yaml(design2,yaml_output_path):
-    """
-    Writes an optimal design configuration into a YAML file.
-
-    Parameters:
-        design2 (list): List of design configurations.
-        yaml_output_path (str): Path to the output YAML file.
-
-    Returns:
-        None. Writes the optimal design configuration to a YAML file.
-"""
-    # Convert design2 to dictionary format
-    design2_dict = {}
-    for i in range(0, len(design2), 2):
-        key = design2[i]
-        values = design2[i + 1]
-        design2_dict[key] = eval(str(values))
-    with open(yaml_output_path, 'w') as outfile:
-        yaml.dump(design2_dict, outfile, default_flow_style=False)
 
 def create_cmap():
     '''
